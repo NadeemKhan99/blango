c58797d3fd33691a4ee8338f02b3a379350e15b2
Nadeem Khan <khannadeen659@gmail.com>
2023-02-25 08:03:28 +0000

    Finish security and passwords

--- blango/settings.py
+++ blango/settings.py
@@ -105,6 +105,13 @@ class Dev(Configuration):
 
   ROOT_URLCONF = 'blango.urls'
 
+  PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.Argon2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
+
   TEMPLATES = [
       {
           'BACKEND': 'django.template.backends.django.DjangoTemplates',
